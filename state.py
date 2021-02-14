3d5441ec81c4da83cd65299063fd02d996b045ca
leon <l>
2021-02-14 16:04:14 -0500

    fixed some weird thing with merge?

--- state.py
+++ state.py
@@ -3,33 +3,18 @@ from player import *
 from tiles import tiles
 
 class State:
-<<<<<<< HEAD
     colors = ['#da00fe', '#fea600', "#fe4138", "#0072BB", "#29b30f", "#00e2e5"]  
     fake_rolls = [(3,4), (1,1)]
     cur_roll = -1 
-    
+
     def get_roll(self):
-=======
-    colors = ['#da00fe', '#fea600', "#fe4138", "#0072BB", "#29b30f", "#00e2e5"]
-    fake_rolls = [(4, 4), (1, 1)]
-    cur_roll = -1
-
-    def get_roll():
->>>>>>> b5e82b30e0c76d24e90cbe54e42a941bd3a20eb7
         State.cur_roll += 1
-
         return State.fake_rolls[cur_roll]
 
-<<<<<<< HEAD
     def process_roll(self):
         (a,b) = self.get_roll()
         self.cur_player() 
-    
-=======
-    def process_roll():
-        (a, b) = get_roll()
 
->>>>>>> b5e82b30e0c76d24e90cbe54e42a941bd3a20eb7
     def __init__(self):
         self.tiles = tiles.tiles
         self.currentTurn = 0
